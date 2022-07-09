04f1d1c34e90add6694ea613fa5b79c2bf291a61
Facundo Acosta <facundoacostayl@outlook.com>
2022-07-09 05:26:45 -0300

    currency values added

--- src/ui/priceBox/PriceBox.tsx
+++ src/ui/priceBox/PriceBox.tsx
@@ -1,21 +1,35 @@
-import styles from './PriceBox.module.scss';
+import styles from "./PriceBox.module.scss";
 
 type Props = {
-    ARS: number,
-    USD: number
-}
+  ARS: number;
+  USD: number;
+};
 
-export const PriceBox: React.FC = ({children}, {ARS, USD}: Props) => {
+export const PriceBox: React.FC = ({ children }, { ARS = 0, USD = 0 }: Props) => {
   return (
     <div className={styles.container}>
-        <div className={styles.arsSide}>
-            <i><img src={require("../../img/argentinaflag.webp")} alt="argentina"/></i>
-            <p>${ARS || 0}</p>
-        </div>
-        <div className={styles.usdSide}>
-            <i><img src={require("../../img/usaflag.webp")} alt="usa"/></i>
-            <p>${USD || 0}</p>
-        </div>
+      <div className={styles.arsSide}>
+        <i>
+          <img src={require("../../img/argentinaflag.webp")} alt="argentina" />
+        </i>
+        <p>
+          {new Intl.NumberFormat("es-AR", {
+            style: "currency",
+            currency: "ARS",
+          }).format(ARS)}
+        </p>
+      </div>
+      <div className={styles.usdSide}>
+        <i>
+          <img src={require("../../img/usaflag.webp")} alt="usa" />
+        </i>
+        <p>
+          {new Intl.NumberFormat("en-US", {
+            style: "currency",
+            currency: "USD",
+          }).format(USD)}
+        </p>
+      </div>
     </div>
-  )
-}
+  );
+};
